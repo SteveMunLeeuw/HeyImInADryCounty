1c26fe0c0ca71ae36c3ccab7316740400bf78851
SteveMunLeeuw <smunleeuw@gmail.com>
2013-09-03 23:14:21 -0500

    renaming to work around google cache

--- js/helloworld/google.js
+++ js/helloworld/google.js
@@ -11,7 +11,7 @@ define(
 
                 var map = new google.maps.Map( mapCanvas, myOptions );	
                 var ctaLayer = new google.maps.KmlLayer({
-                    url: 'https://raw.github.com/SteveMunLeeuw/HeyImInADryCounty/master/texasCounties.kml'
+                    url: 'https://raw.github.com/SteveMunLeeuw/HeyImInADryCounty/master/texasCounties.kml?dummy=1'
                 });
                 ctaLayer.setMap(map);
 
