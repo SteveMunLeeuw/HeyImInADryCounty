73b3f00657a4620908f2ef01c97ca4a6c4644c07
SteveMunLeeuw <smunleeuw@gmail.com>
2013-09-07 12:14:00 -0500

    adding state level

--- js/helloworld/google.js
+++ js/helloworld/google.js
@@ -11,7 +11,7 @@ define(
 
                 var map = new google.maps.Map( mapCanvas, myOptions );	
                 var ctaLayer = new google.maps.KmlLayer({
-                    url: 'https://raw.github.com/SteveMunLeeuw/HeyImInADryCounty/master/texasCountyLevel.kml?a=2'
+                    url: 'https://raw.github.com/SteveMunLeeuw/HeyImInADryCounty/master/texasCountyLevel.kml?a=3'
                 });
                 ctaLayer.setMap(map);
 
